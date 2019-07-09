99044c6d6c4154b5c59a6b46f4f4779682303211
libinyl <1102109100@qq.com>
2019-07-09 19:19:25 +0800

    keep master a test env.

--- algorithm/daily_test/search/binarySearch.py
+++ algorithm/daily_test/search/binarySearch.py
@@ -2,13 +2,4 @@ from typing import List
 
 
 def binary_search(arr: List[int], x: int, begin: int, end: int):
-    l, r = begin, end
-    while (l < r):
-        mid = l+int((r-l)/2)
-        if arr[mid] < x:
-            l = mid + 1
-        else:
-            r = mid
-    if l == end:
-        raise ValueError
-    return l
+    return
